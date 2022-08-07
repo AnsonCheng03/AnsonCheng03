20c096bc5916f3a4850a103931634cd9893ecf0c
Anson Cheng <Anson.12666@icloud.com>
2022-08-08 01:49:42 +0800

    Add Animation and Reduce onScroll usage

--- script.js
+++ script.js
@@ -1,51 +1,14 @@
 var currentlyProcessingCall = false;
 let isChromium = !!window.chrome;
-
-window.scrollTo(0, 0);
-
 const objparent = document.querySelector('.Skills');
 const obj = document.querySelector('.skilllists');
 const objtotrans = document.querySelector('.skilllists .flexbox');
 const nextsection = document.querySelector('.Contact');
 const navtxt = document.querySelector('nav h1');
 
-function delayfunction(ms, callback) {
-    if (currentlyProcessingCall) return;
-    currentlyProcessingCall = true;
-    eval(callback)();
-    setTimeout(function(){ currentlyProcessingCall = false; },ms);
-  }
-
-function checkheaderchange() {
-    if (objparent.getBoundingClientRect().top <= 0) {
-        if (navtxt.innerHTML !== "Skills") {
-            //navtxt.style.fontSize = 0;
-            navtxt.style.opacity = 0.2;
-            setTimeout(function () {
-                navtxt.style.borderColor = "#4831d4";
-                navtxt.innerHTML = "Skills";
-                //navtxt.style.fontSize = "1.5rem";
-                navtxt.style.opacity = 1;
-            }, 350);
-        }
-    } else {
-        if (navtxt.innerHTML !== "Education &amp; Working Exp.") {
-            //navtxt.style.fontSize = 0;
-            navtxt.style.opacity = 0.2;
-            setTimeout(function () {
-                navtxt.innerHTML = "Education & Working Exp.";
-                navtxt.style.borderColor = "#47bcd4";
-                //navtxt.style.fontSize = "1.5rem";
-                navtxt.style.opacity = 1;
-            }, 350);
-        }
-    }
-}
-
-
 function scrollbox() {
+    console.log("a")
     /* Trans X when scroll*/
-
     const objwidth = objtotrans.childElementCount * objtotrans.firstElementChild.offsetWidth;
     const distancecanmove = nextsection.getBoundingClientRect().top - objparent.getBoundingClientRect().top;
     const DistanceMoved = obj.getBoundingClientRect().top - objparent.getBoundingClientRect().top - 70 * window.innerHeight / 100;
@@ -53,16 +16,18 @@ function scrollbox() {
     var MoveDist = DistanceMoved / (distancecanmove - 70 / 100 * window.innerHeight) * (objwidth);
     if (MoveDist < 0) MoveDist = 0;
     objtotrans.style.transform = "translateX(-" + MoveDist + "px)";
-
-    if(isChromium)
-        delayfunction(750, checkheaderchange);
-    else 
-        checkheaderchange()
 }
 
-window.addEventListener('resize', scrollbox);
-window.addEventListener('scroll', scrollbox);
+window.addEventListener("mousemove", (e) => {
+    document.querySelector("#cursor").style.display = "unset";
+    document.querySelector("#cursor").style.setProperty('--cursorpx', e.clientX + 'px, ' + e.clientY + 'px');
+});
+document.documentElement.addEventListener('mouseleave', () => {
+    document.querySelector("#cursor").style.display = "none";
+})
 
+
+window.scrollTo(0, 0);
 setTimeout(function () {
     document.querySelector('.loader .background').style.transform = "rotate(360deg) scale(0)";
 }, 500);
@@ -74,13 +39,44 @@ setTimeout(function () {
 setTimeout(function () {
     document.querySelector('.loader').remove();
 }, 1500);
+if (!/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)) {
+    document.querySelector('.warning .PCV').style.display = "none";
+}
 
-
-window.addEventListener("mousemove", (e) => {
-    document.querySelector("#cursor").style.display = "unset";
-    document.querySelector("#cursor").style.setProperty('--cursorpx', e.clientX + 'px, ' + e.clientY + 'px');
+const observer = new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            entry.target.setAttribute('enterscreen', '');
+        } else {
+            entry.target.removeAttribute('enterscreen');
+        }
+    }
 });
+for (const item of document.querySelectorAll('.card-content')) {
+    observer.observe(item);
+}
 
-document.documentElement.addEventListener('mouseleave', () => {
-    document.querySelector("#cursor").style.display = "none";
-})
+new IntersectionObserver((entries) => {
+    for (const entry of entries) {
+        if (entry.isIntersecting) {
+            window.removeEventListener('resize', scrollbox);
+            window.removeEventListener('scroll', scrollbox);
+            navtxt.style.opacity = 0.2;
+            setTimeout(function () {
+                navtxt.innerHTML = "Education & Working Exp.";
+                navtxt.style.borderColor = "#47bcd4";
+                navtxt.style.opacity = 1;
+            }, 350);
+        } else {
+
+            window.addEventListener('resize', scrollbox);
+            window.addEventListener('scroll', scrollbox);
+            navtxt.style.opacity = 0.2;
+            setTimeout(function () {
+                navtxt.style.borderColor = "#4831d4";
+                navtxt.innerHTML = "Skills";
+                navtxt.style.opacity = 1;
+            }, 350);
+        }
+    }
+}).observe(document.querySelector('.Education'));
