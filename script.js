b25c867daa1a6ca1e1222d85dc3ca3902dce0b75
Anson Cheng <Anson.12666@icloud.com>
2022-08-07 23:33:30 +0800

    Fix onScroll flickering bug on Webkit Browsers
    still buggy on chromium browsers

--- script.js
+++ script.js
@@ -1,47 +1,63 @@
-function scrollbox() {
-    /* Trans X when scroll*/
-    const objparent = document.querySelector('.Skills');
-    const obj = document.querySelector('.skilllists');
-    const objtotrans = document.querySelector('.skilllists .flexbox');
-    const nextsection = document.querySelector('.Contact');
+var currentlyProcessingCall = false;
+const objparent = document.querySelector('.Skills');
+const obj = document.querySelector('.skilllists');
+const objtotrans = document.querySelector('.skilllists .flexbox');
+const nextsection = document.querySelector('.Contact');
+const navtxt = document.querySelector('nav h1');
 
-    const objwidth = objtotrans.lastElementChild.getBoundingClientRect().right - objtotrans.firstElementChild.getBoundingClientRect().left;
-    const distancecanmove = nextsection.getBoundingClientRect().top - objparent.getBoundingClientRect().top;
-    const DistanceMoved = obj.getBoundingClientRect().top - objparent.getBoundingClientRect().top - 70 * window.innerHeight / 100;
+function delayfunction(ms, callback) {
+    if (currentlyProcessingCall) return;
+    currentlyProcessingCall = true;
+    eval(callback)();
+    setTimeout(function(){ currentlyProcessingCall = false; },ms);
+  }
 
-    var MoveDist = DistanceMoved / (distancecanmove - 70 / 100 * window.innerHeight) * (objwidth + 4 / 3 * window.innerWidth);
-    if (MoveDist < 0) MoveDist = 0;
-    console.log()
-    objtotrans.style.transform = "translateX(-" + MoveDist + "px)";
-
-    const navtxt = document.querySelector('nav h1');
+function checkheaderchange() {
+    console.log(navtxt.innerHTML);
     if (objparent.getBoundingClientRect().top <= 0) {
         if (navtxt.innerHTML !== "Skills") {
-            navtxt.style.fontSize = 0;
+            //navtxt.style.fontSize = 0;
             navtxt.style.opacity = 0.2;
             setTimeout(function () {
                 navtxt.style.borderColor = "#4831d4";
                 navtxt.innerHTML = "Skills";
-                navtxt.style.fontSize = "1.5rem";
+                //navtxt.style.fontSize = "1.5rem";
                 navtxt.style.opacity = 1;
             }, 350);
         }
     } else {
         if (navtxt.innerHTML !== "Education &amp; Working Exp.") {
-            navtxt.style.fontSize = 0;
+            //navtxt.style.fontSize = 0;
             navtxt.style.opacity = 0.2;
             setTimeout(function () {
-                navtxt.style.borderColor = "#47bcd4";
                 navtxt.innerHTML = "Education & Working Exp.";
-                navtxt.style.fontSize = "1.5rem";
+                navtxt.style.borderColor = "#47bcd4";
+                //navtxt.style.fontSize = "1.5rem";
                 navtxt.style.opacity = 1;
             }, 350);
         }
     }
 }
 
-document.addEventListener('resize', scrollbox);
-document.addEventListener('scroll', scrollbox);
+
+function scrollbox() {
+    /* Trans X when scroll*/
+
+    const objwidth = objtotrans.childElementCount * objtotrans.firstElementChild.offsetWidth;
+    const distancecanmove = nextsection.getBoundingClientRect().top - objparent.getBoundingClientRect().top;
+    const DistanceMoved = obj.getBoundingClientRect().top - objparent.getBoundingClientRect().top - 70 * window.innerHeight / 100;
+
+    var MoveDist = DistanceMoved / (distancecanmove - 70 / 100 * window.innerHeight) * (objwidth);
+    if (MoveDist < 0) MoveDist = 0;
+    objtotrans.style.transform = "translateX(-" + MoveDist + "px)";
+
+    delayfunction(500, checkheaderchange);
+}
+
+window.addEventListener('resize', scrollbox);
+window.addEventListener('scroll', scrollbox);
+
+document.querySelector('body').scrollIntoView()
 
 setTimeout(function () {
     document.querySelector('.loader .background').style.transform = "rotate(360deg) scale(0)";
