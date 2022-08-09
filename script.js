588ef5618b5e5f522742443499e1ff15b3ca9404
Anson Cheng <Anson.12666@icloud.com>
2022-08-09 22:34:43 +0800

    Modi Animation

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-let isChromium = !!window.chrome;
 const ball = document.querySelector('.ball');
 const descmelst = document.querySelector('.descme');
 const objtotrans = document.querySelector('.skilllists .flexbox');
@@ -46,24 +45,24 @@ new IntersectionObserver((entries) => {
         if (entry.isIntersecting) {
             window.removeEventListener('resize', scrollbox);
             window.removeEventListener('scroll', scrollbox);
-            if (!isChromium) navtxt.style.fontSize = 0;
+            navtxt.style.fontSize = 0;
             navtxt.style.opacity = 0.2;
             setTimeout(function () {
                 navtxt.innerHTML = "Education & Working Exp.";
                 navtxt.style.borderColor = "#47bcd4";
                 navtxt.style.opacity = 1;
-                if (!isChromium) navtxt.style.fontSize = "1.5rem";
+                navtxt.style.fontSize = "1.5rem";
             }, 350);
         } else {
             window.addEventListener('resize', scrollbox);
             window.addEventListener('scroll', scrollbox);
-            if (!isChromium) navtxt.style.fontSize = 0;
+            navtxt.style.fontSize = 0;
             navtxt.style.opacity = 0.2;
             setTimeout(function () {
                 navtxt.style.borderColor = "#4831d4";
                 navtxt.innerHTML = "Skills";
                 navtxt.style.opacity = 1;
-                if (!isChromium) navtxt.style.fontSize = "1.5rem";
+                navtxt.style.fontSize = "1.5rem";
             }, 350);
         }
     }
@@ -72,3 +71,50 @@ new IntersectionObserver((entries) => {
 
 
 
+
+/*
+var timer = null;
+
+if (!!window.chrome) {
+
+
+    function scrolldownsimu(e) {
+
+        console.log(timer)
+
+
+        
+
+        if (timer !== null) {
+            clearTimeout(timer);
+        } else {
+            window.scrollBy({
+                top: (e.deltaY < 0 ? -100 : 100),
+                behavior: 'smooth'
+            });
+        }
+
+        timer = setTimeout(() => {
+            console.log("aa")
+        }, 150);
+        
+    }
+    window.addEventListener('keydown', (e) => scrolldownsimu(e));
+
+    
+
+
+    let supportsPassive = false;
+    try {
+        window.addEventListener("test", null, Object.defineProperty({}, 'passive', {
+            get: function () { supportsPassive = true; }
+        }));
+    } catch (e) { }
+    const wheelOpt = supportsPassive ? { passive: false } : false;
+    const wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+    window.addEventListener('DOMMouseScroll', (e) => scrolldownsimu(e), false);
+    window.addEventListener(wheelEvent, (e) => scrolldownsimu(e), wheelOpt);
+    window.addEventListener('touchmove', (e) => scrolldownsimu(e), wheelOpt);
+}
+*/
