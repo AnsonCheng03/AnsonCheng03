d2aa21690b9da11d5fadcfdd610bb5c21b7b0b56
Anson Cheng <Anson.12666@icloud.com>
2024-03-03 20:38:18 +0800

    chore: create project fade in

--- projects.js
+++ projects.js
@@ -8,3 +8,37 @@ gsap.from(".Project", {
     end: "50%",
   },
 });
+
+gsap.to(".Project .sectionTitle", {
+  top: "35vh",
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".Project",
+    scrub: true,
+    start: "top",
+    end: "25%  -50%",
+  },
+});
+
+gsap.from(".Project .sectionTitle .imageContainer", {
+  height: "150vh",
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".Project",
+    scrub: true,
+    start: "top  -50%",
+    end: "25%",
+  },
+});
+
+gsap.to(".Project .sectionTitle h1", {
+  autoAlpha: 0,
+  y: -100,
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".Project",
+    scrub: true,
+    start: "top -50%",
+    end: "25%",
+  },
+});
