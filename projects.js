0e6078cc274ba22fda0429e4f32299d691bfb5cf
Anson Cheng <Anson.12666@icloud.com>
2024-03-26 20:53:44 +0800

    feat: create mobile view scroll

--- projects.js
+++ projects.js
@@ -10,35 +10,45 @@ gsap.from(".Project", {
 });
 
 gsap.to(".Project .sectionTitle", {
-  top: "35vh",
-  ease: "power2.inOut",
+  width: "50vw",
   scrollTrigger: {
     trigger: ".Project",
     scrub: true,
-    start: "top -100%",
-    end: "25%  -50%",
-  },
-});
-
-gsap.to(".Project .sectionTitle .imageContainer .case", {
-  height: "calc(80vw / 16 * 9)",
-  ease: "power2.inOut",
-  scrollTrigger: {
-    trigger: ".Project",
-    scrub: true,
-    start: `top top`,
+    start: "20%",
     end: "25%",
   },
 });
 
-gsap.to(".Project .sectionTitle h1", {
+gsap.from(".Project .sectionTitle .imageContainer .case", {
+  width: "110vw",
+  ease: "power2.inOut",
+  scrollTrigger: {
+    trigger: ".Project",
+    scrub: true,
+    start: `15% 20%`,
+    end: "25%",
+  },
+});
+
+// gsap.to(".Project .sectionTitle h1", {
+//   autoAlpha: 0,
+//   y: -100,
+//   ease: "power2.inOut",
+//   scrollTrigger: {
+//     trigger: ".Project",
+//     scrub: true,
+//     start: `10% 15%`,
+//     end: "20%",
+//   },
+// });
+
+gsap.from(".Project .projectDescription", {
   autoAlpha: 0,
-  y: -100,
   ease: "power2.inOut",
   scrollTrigger: {
     trigger: ".Project",
     scrub: true,
-    start: `top -100%`,
-    end: "25%",
+    start: `22%`,
+    end: "27.5%",
   },
 });
